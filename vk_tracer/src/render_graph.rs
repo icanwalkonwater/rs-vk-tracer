7ccd59276ce84ec0d1d4487c14fca43b966e9adc
Lucas Malandrino <lucas.malandrino@gmail.com>
2021-06-24 17:27:23 +0200

    Cleanup render graph tests

--- vk_tracer/src/render_graph.rs
+++ vk_tracer/src/render_graph.rs
@@ -18,34 +18,16 @@ mod tests {
     use ash::vk;
 
     #[test]
-    fn yolo() {
+    fn test_graph_simple() {
         let mut graph = RenderGraph::new();
 
         graph.register_attachment(
-            "Albedo",
+            "Swapchain",
             AttachmentInfo {
                 size: AttachmentSize::SwapchainRelative,
-                format: vk::Format::R8G8B8A8_UNORM,
+                format: vk::Format::R8G8B8_UNORM,
                 transient: false,
-            },
-        );
-
-        graph.register_attachment(
-            "Position",
-            AttachmentInfo {
-                size: AttachmentSize::SwapchainRelative,
-                format: vk::Format::R16G16B16A16_SFLOAT,
-                transient: false,
-            },
-        );
-
-        graph.register_attachment(
-            "Normal",
-            AttachmentInfo {
-                size: AttachmentSize::SwapchainRelative,
-                format: vk::Format::R16G16B16A16_SFLOAT,
-                transient: false,
-            },
+            }
         );
 
         graph.register_attachment(
@@ -54,78 +36,174 @@ mod tests {
                 size: AttachmentSize::SwapchainRelative,
                 format: vk::Format::D32_SFLOAT,
                 transient: true,
-            },
+            }
         );
 
-        graph.register_attachment(
-            "ShadowMap",
-            AttachmentInfo {
-                size: AttachmentSize::Fixed(vk::Extent3D {
-                    width: 1024,
-                    height: 1024,
-                    depth: 1,
-                }),
-                format: vk::Format::D32_SFLOAT,
-                transient: false,
-            },
-        );
-
-        graph.register_attachment(
-            "Shaded",
-            AttachmentInfo {
-                size: AttachmentSize::SwapchainRelative,
-                format: vk::Format::R8G8B8_SRGB,
-                transient: false,
-            },
-        );
-
-        graph.register_attachment(
-            "ToneMap",
-            AttachmentInfo {
-                size: AttachmentSize::Fixed(vk::Extent3D {
-                    width: 256,
-                    height: 256,
-                    depth: 1,
-                }),
-                format: vk::Format::R8G8B8_UNORM,
-                transient: false,
-            },
-        );
-
-        graph.register_attachment(
-            "Swapchain",
-            AttachmentInfo {
-                size: AttachmentSize::SwapchainRelative,
-                format: vk::Format::R8G8B8_SRGB,
-                transient: false,
-            },
-        );
-
-        graph
-            .new_pass("GBuffer", RenderPassType::Graphics)
-            .add_color_attachment("Albedo")
-            .add_color_attachment("Position")
-            .add_color_attachment("Normal")
+        graph.new_pass("Forward", RenderPassType::Graphics)
+            .add_color_attachment("Swapchain")
             .set_depth_stencil_output("Depth");
 
-        graph
-            .new_pass("Shadow Pass", RenderPassType::Graphics)
-            .set_depth_stencil_output("ShadowMap");
+        graph.set_back_buffer("Swapchain");
 
-        graph
-            .new_pass("Lighting", RenderPassType::Graphics)
-            .add_color_attachment("Shaded")
-            .add_input_attachment("Albedo")
-            .add_input_attachment("Position")
-            .add_input_attachment("Normal")
-            .add_input_attachment("Depth")
-            .add_image_input("ShadowMap");
+        let baked_graph = BakedRenderGraph::bake(graph).unwrap();
 
-        graph
-            .new_pass("Post Processing", RenderPassType::Graphics)
-            .add_color_attachment("Swapchain")
-            .add_input_attachment("Shaded")
-            .add_image_input("ToneMap");
+        // Check attachments
+        {
+            // Swapchain
+            assert_eq!(baked_graph.resources[0], BakedRenderResource {
+                size: AttachmentSize::SwapchainRelative,
+                format: vk::Format::R8G8B8_UNORM,
+            });
+            // Depth
+            assert_eq!(baked_graph.resources[1], BakedRenderResource {
+                size: AttachmentSize::SwapchainRelative,
+                format: vk::Format::D32_SFLOAT,
+            });
+        }
+
+        // Check pass
+        assert_eq!(baked_graph.passes[0], BakedRenderPass {
+            ty: RenderPassType::Graphics,
+            color_attachments: vec![0],
+            input_attachments: vec![],
+            depth_attachment: Some(1),
+            image_inputs: vec![],
+            barriers: [
+                (0, BakedResourceBarrier {
+                    src_stage: vk::PipelineStageFlags::TOP_OF_PIPE,
+                    dst_stage: vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT,
+                    src_access: vk::AccessFlags::empty(),
+                    dst_access: vk::AccessFlags::COLOR_ATTACHMENT_WRITE,
+                    old_layout: vk::ImageLayout::UNDEFINED,
+                    new_layout: vk::ImageLayout::PRESENT_SRC_KHR,
+                }),
+                (1, BakedResourceBarrier {
+                    src_stage: vk::PipelineStageFlags::TOP_OF_PIPE,
+                    dst_stage: vk::PipelineStageFlags::EARLY_FRAGMENT_TESTS | vk::PipelineStageFlags::LATE_FRAGMENT_TESTS,
+                    src_access: vk::AccessFlags::empty(),
+                    dst_access: vk::AccessFlags::DEPTH_STENCIL_ATTACHMENT_READ | vk::AccessFlags::DEPTH_STENCIL_ATTACHMENT_WRITE,
+                    old_layout: vk::ImageLayout::UNDEFINED,
+                    new_layout: vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+                }),
+            ].iter().cloned().collect(),
+        })
+    }
+
+    #[test]
+    fn test_graph_complex_deferred_shadow_post() {
+        let mut graph = RenderGraph::new();
+
+        // Add attachments
+        {
+            graph.register_attachment(
+                "Albedo",
+                AttachmentInfo {
+                    size: AttachmentSize::SwapchainRelative,
+                    format: vk::Format::R8G8B8A8_UNORM,
+                    transient: false,
+                },
+            );
+
+            graph.register_attachment(
+                "Position",
+                AttachmentInfo {
+                    size: AttachmentSize::SwapchainRelative,
+                    format: vk::Format::R16G16B16A16_SFLOAT,
+                    transient: false,
+                },
+            );
+
+            graph.register_attachment(
+                "Normal",
+                AttachmentInfo {
+                    size: AttachmentSize::SwapchainRelative,
+                    format: vk::Format::R16G16B16A16_SFLOAT,
+                    transient: false,
+                },
+            );
+
+            graph.register_attachment(
+                "Depth",
+                AttachmentInfo {
+                    size: AttachmentSize::SwapchainRelative,
+                    format: vk::Format::D32_SFLOAT,
+                    transient: true,
+                },
+            );
+
+            graph.register_attachment(
+                "ShadowMap",
+                AttachmentInfo {
+                    size: AttachmentSize::Fixed(vk::Extent3D {
+                        width: 1024,
+                        height: 1024,
+                        depth: 1,
+                    }),
+                    format: vk::Format::D32_SFLOAT,
+                    transient: false,
+                },
+            );
+
+            graph.register_attachment(
+                "Shaded",
+                AttachmentInfo {
+                    size: AttachmentSize::SwapchainRelative,
+                    format: vk::Format::R8G8B8_SRGB,
+                    transient: false,
+                },
+            );
+
+            graph.register_attachment(
+                "ToneMap",
+                AttachmentInfo {
+                    size: AttachmentSize::Fixed(vk::Extent3D {
+                        width: 256,
+                        height: 256,
+                        depth: 1,
+                    }),
+                    format: vk::Format::R8G8B8_UNORM,
+                    transient: false,
+                },
+            );
+
+            graph.register_attachment(
+                "Swapchain",
+                AttachmentInfo {
+                    size: AttachmentSize::SwapchainRelative,
+                    format: vk::Format::R8G8B8_SRGB,
+                    transient: false,
+                },
+            );
+        }
+
+        // Add passes
+        {
+            graph
+                .new_pass("GBuffer", RenderPassType::Graphics)
+                .add_color_attachment("Albedo")
+                .add_color_attachment("Position")
+                .add_color_attachment("Normal")
+                .set_depth_stencil_output("Depth");
+
+            graph
+                .new_pass("Shadow Pass", RenderPassType::Graphics)
+                .set_depth_stencil_output("ShadowMap");
+
+            graph
+                .new_pass("Lighting", RenderPassType::Graphics)
+                .add_color_attachment("Shaded")
+                .add_input_attachment("Albedo")
+                .add_input_attachment("Position")
+                .add_input_attachment("Normal")
+                .add_input_attachment("Depth")
+                .add_image_input("ShadowMap");
+
+            graph
+                .new_pass("Post Processing", RenderPassType::Graphics)
+                .add_color_attachment("Swapchain")
+                .add_input_attachment("Shaded")
+                .add_image_input("ToneMap");
+        }
 
         graph.set_back_buffer("Swapchain");
 
@@ -133,6 +211,8 @@ mod tests {
 
         // Check generated resources
         {
+            assert_eq!(baked_graph.resources.len(), 8);
+
             // Albedo
             assert_eq!(
                 baked_graph.resources[0],
@@ -209,6 +289,8 @@ mod tests {
 
         // Check generated passes
         {
+            assert_eq!(baked_graph.passes.len(), 4);
+
             // Graphics
             let color_first_pass_barrier = BakedResourceBarrier {
                 src_stage: vk::PipelineStageFlags::TOP_OF_PIPE,
